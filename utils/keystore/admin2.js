66d3564a66d731c4e2d758ab22783cf838c9af96
madhavaggar <aggarwal.madhav@gmail.com>
2020-09-28 13:01:06 +0530

    Netlify config

---
+++ utils/keystore/admin2.js
@@ -0,0 +1,8 @@
+module.exports = 
+    {
+        "publicKey": "edpkuh61V1ox8YFknfphNu46KEhDmUtVqkbMQTXAY8xBnneiASwyCu",
+        "privateKey": "edskRpbQU3qWh4eVJUUXnXz7oxhAiNh43gsuDHc7MH4USATbuDjLHpaqEUixfevPszEz2kDQiFEZDexcDQKTVcbz2v8zHEtBWD",
+        "publicKeyHash": "tz1iMnChC6v9noCFRTt5VuUrgWqNJAjwQCW1",
+        "seed": "",
+        "storeType": 1
+      };
